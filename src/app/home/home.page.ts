5d7c097829ebe6727287aaab602aac34bbfd41d4
DraKers1994 <kerlisnieto@gmail.com>
2023-01-12 21:25:00 -0500

    Clase12-01

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  slideOpt = {
+    initialSlide: 0, //slide inicial
+    slidesPerView: 1, //configuramos un slide por vista
+    centerSlides: true, // que slides enten centrados
+    speed: 400 //velocidad de los movimientos del slide
+  }
   constructor() {}
 
 }
